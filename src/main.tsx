162f051b083f4a5c7c1eead6abdca06ca422bda5
Ashish <ajeswani80@gmail.com>
2023-09-08 19:56:27 +0530

    App Bug Fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import {ProductStore} from "./stores/ProductStore.ts";
 import {RootStore} from "./stores/RootStore.ts";
-import {CartStore} from "./stores/CartStore.ts";
 
 
 
 const root = new RootStore()
-const productStore = new ProductStore(root)
-const cartStore = new CartStore(root)
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
